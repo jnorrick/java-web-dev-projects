9a06d9f4c2d83f88b467a81cc2d4700f0c5b5484
Jessica Orrick <jessica.orrick@outlook.com>
2023-12-03 21:15:01 -0600

    Started ArrayListPractice

---
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/launchcode/ArrayListPractice.java
@@ -0,0 +1,17 @@
+package org.launchcode;
+
+import java.util.ArrayList;
+
+public class ArrayListPractice {
+    public static void main(String[] args) {}
+        public static int sumEven(ArrayList<Integer> arr) {
+            int total = 0;
+            for (int integer : arr) {
+                if (integer % 2 == 0) {
+                    total += integer;
+                }
+            }
+            return total;
+        }
+
+}
