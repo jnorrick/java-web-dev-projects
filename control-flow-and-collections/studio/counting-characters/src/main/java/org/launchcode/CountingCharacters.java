3d8a63e7732e3085ec000f1a1348414292117636
Jessica Orrick <jessica.orrick@outlook.com>
2023-12-05 13:56:03 -0600

    studio update completed and bonus mission case-insensitive

--- control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/CountingCharacters.java
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/CountingCharacters.java
@@ -1,24 +1,26 @@
 package org.launchcode;
 
-
 import java.util.HashMap;
+import java.util.Map;
 
 public class CountingCharacters {
     public static void main(String[] args) {
-        HashMap<Character, Integer> characterCount = new HashMap<>() {
-            String myString = "Hello World";
-            char[] chVar = myString.toLowerCase().toCharArray();
-
-            for (char c : chVar) {
-                if (characterCount.containsKey(c)) {
-                    characterCount.put(c, characterCount.get(c) + 1);
-                } else {
-                    characterCount.put(c, 1);
-                }
+        String myString = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        char[] charArray = myString.toLowerCase().toCharArray();
+        HashMap<Character, Integer> characterCount = new HashMap<>();
+        for (char letter : charArray) {
+            if (characterCount.containsKey(letter)) {
+                characterCount.put(letter, characterCount.get(letter) + 1);
+            } else {
+                characterCount.put(letter, 1);
             }
-        };
+        }
+        for (Map.Entry<Character, Integer> count : characterCount.entrySet()) {
+            System.out.println(count.getKey() + ": " + count.getValue());
+        }
     }
-}
+};
+
 
 
 //for (Map.Entry<Character, Integer> c : characterCount.EntrySet()) {
