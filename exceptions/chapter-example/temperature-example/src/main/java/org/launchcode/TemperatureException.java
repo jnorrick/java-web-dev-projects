b5148dcaa3ce45281ffd84a8e0b2674aacf4cfbb
Jessica Orrick <jessica.orrick@outlook.com>
2024-01-11 10:18:49 -0600

    Completed Walkthrough Temperature and Temperature Exception

--- exceptions/chapter-example/temperature-example/src/main/java/org/launchcode/TemperatureException.java
+++ exceptions/chapter-example/temperature-example/src/main/java/org/launchcode/TemperatureException.java
@@ -1,5 +1,10 @@
 package org.launchcode;
 
-public class TemperatureException {
-    // Write code here!
+public class TemperatureException extends Exception {
+    public TemperatureException (String message) {
+        super(message);
+    }
+
 }
+
+
