a7781412b1cc60ec02c5130d47c76f068a8d0d05
Jessica Orrick <jessica.orrick@outlook.com>
2024-01-11 16:00:32 -0600

    Exception Exercises started

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+        int x = 5;
+        int y = 0;
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -15,13 +17,19 @@ public class Main {
         // Test out your CheckFileExtension() function!
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static int Divide(int x, int y) {
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("Cannot divide by 0!");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
+        return x/y;
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
-    }
+//    public static int CheckFileExtension(String fileName) {
+//        return 0; //fix so it stops breaking :D
+//    }
+
 }
